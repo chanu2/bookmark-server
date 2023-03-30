4ea32b4d75e2cbf71228d789e3115e87299c793f
chanu2 <mdsoo55828@gmail.com>
2023-03-30 18:57:54 +0900

    3.30 modify

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO profile_image (image_url)
+VALUES ('https://checkmark.s3.ap-northeast-2.amazonaws.com/turtle.png'),
+       ('https://checkmark.s3.ap-northeast-2.amazonaws.com/t-rex.png'),
+       ('https://checkmark.s3.ap-northeast-2.amazonaws.com/shark.png'),
+       ('https://checkmark.s3.ap-northeast-2.amazonaws.com/giraffe.png'),
+       ('https://checkmark.s3.ap-northeast-2.amazonaws.com/crab.png');
+
+
+INSERT INTO mood_image (mood_image_url,mood_name)
+VALUES ('https://checkmark.s3.ap-northeast-2.amazonaws.com/rolling.png','매우 좋음'),
+       ('https://checkmark.s3.ap-northeast-2.amazonaws.com/smile.png','좋음'),
+       ('https://checkmark.s3.ap-northeast-2.amazonaws.com/susim.png','보통'),
+       ('https://checkmark.s3.ap-northeast-2.amazonaws.com/worried.png','나쁨'),
+       ('https://checkmark.s3.ap-northeast-2.amazonaws.com/angry.png','매우나쁨');
